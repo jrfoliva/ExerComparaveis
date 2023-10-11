b690c53fc36ddbd5dfa6e10f43c03d9c1f3d2911
Junior Oliva <jrfoliva@gmail.com>
2023-10-10 23:56:34 -0300

    Add project files.

---
+++ Entites/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System;
+namespace ExerComparaveis.Entites
+{
+    internal class Employee : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csvString)
+        {
+            string[] vet = csvString.Split(',');
+            Name = vet[0];
+            Salary = double.Parse(vet[1], CultureInfo.InvariantCulture);
+        }
+        public override string ToString()
+        {
+            return Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (!(obj is Employee))
+            {
+                throw new ArgumentException("Comparing error: argument is not an Employee.");
+            }
+            Employee other = obj as Employee; //casting de obj para Employee
+            //Pode-se comparar qualquer atributo da classe.
+            return Salary.CompareTo(other.Salary);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq.Expressions;
+using ExerComparaveis.Entites;
+
+namespace ExerComparaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ler arquivo, ordenar e exibir a lista ordenada
+            string path = @"c:\temp\in.txt";
+            /*
+            try
+            {
+                List<string> list = new List<string>();
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(sr.ReadLine());
+                    }
+                }
+                list.Sort();
+                foreach (string s in list)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);
+            }
+            */
+
+            // Outro problema, ler um arquivo contendo funcionários(nome e salário) no formato .csv
+            // Depois ordenar a lista por nome e mostrar o resultado na tela.
+
+            try
+            {
+                List<Employee> list = new List<Employee>();
+                using (StreamReader sr = File.OpenText(path)) 
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                }
+                list.Sort(); //Falha ao comparar dois elementos no array
+                             //sendo necessário implementar IComparable na classe Employee
+                foreach (Employee emp in list)
+                {
+                    Console.WriteLine(emp);
+                }
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
